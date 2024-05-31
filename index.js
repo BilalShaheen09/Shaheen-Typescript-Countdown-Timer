61b9391f950954550c494509e6a961e45ecd1e50
Bilal Shaheen <161133519+BilalShaheen09@users.noreply.github.com>
2024-05-31 22:26:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+// Countdown Timer Project
+import chalk from "chalk";
+// Function for countdown timer
+function* countdownTimer(seconds) {
+    while (seconds > 0) {
+        yield seconds;
+        seconds--;
+    }
+}
+// Counter timing initialization
+let timerIterator = countdownTimer(10);
+// Function to create a countdown timer
+function displayCountdown() {
+    let result = timerIterator.next();
+    if (!result.done) {
+        console.log(chalk.greenBright(`Remaining Seconds: ${result.value}`));
+        setTimeout(displayCountdown, 1000); // 1 second = 1000 milliseconds
+    }
+    else {
+        console.log(chalk.whiteBright("Countdown Completed!"));
+    }
+}
+// Invoke
+displayCountdown();
